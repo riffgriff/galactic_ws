21cee6dd1a13e2729a7196a1b7a6914bcfd4cba3
Poorvaja Veera Balaji Kumar <38404767+Poorvaja2799@users.noreply.github.com>
2026-03-09 13:12:12 -0600

    Include wayPoints.txt in setup.py data files
    Add wayPoints.txt to data files for installation

--- src/galactic_navigate_to_goal/setup.py
+++ src/galactic_navigate_to_goal/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name, ['galactic_navigate_to_goal/wayPoints.txt']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
