ac7049888768ab39f594a6a3a01b98e971a6e99d
Poorvaja Veera Balaji Kumar <38404767+Poorvaja2799@users.noreply.github.com>
2026-03-09 12:06:26 -0600

    Update launch nodes to use galactic_navigate_to_goal

---
+++ launch/navigate_to_goal_launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='galactic_navigate_to_goal',
+            executable='go_to_goal',
+            name='go_to_goal'
+        ),
+        Node(
+            package='galactic_navigate_to_goal',
+            executable='get_object_range',
+            name='get_object_range'
+        ),
+        Node(
+            package='galactic_navigate_to_goal',
+            executable='drive_carefully',
+            name='drive_carefully',
+            parameters=[
+                {'kp_r' : 1.8},
+                {'ki_r' : 0.0},
+                {'kd_r' : 0.0},
+                {'kp_t' : 0.5},
+                {'ki_t' : 0.0},
+                {'kd_t' : 0.0},
+            ]
+        ),
+    ])
