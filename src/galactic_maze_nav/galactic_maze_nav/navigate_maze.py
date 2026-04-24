2ccc074a3f8026017397a2601e124e4ac18dd187
riffgriff <griffinmartin02@gmail.com>
2026-04-24 18:11:25 -0400

    param tuning

--- src/galactic_maze_nav/galactic_maze_nav/navigate_maze.py
+++ src/galactic_maze_nav/galactic_maze_nav/navigate_maze.py
@@ -50,7 +50,7 @@ class MazeNavigator(Node):
 		self.declare_parameter('model_path', str(default_model_path))
 		self.declare_parameter('knn_k', 7)
 		self.declare_parameter('linear_speed', 0.10)
-		self.declare_parameter('angular_speed', 0.8)
+		self.declare_parameter('angular_speed', 0.6)
 		self.declare_parameter('wall_stop_dist', 0.55)
 		self.declare_parameter('wall_observe_max', 0.70)
 		self.declare_parameter('turn_tolerance_deg', 1.0)
