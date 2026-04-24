68c8b2fe031761b7e46d0fe755f45a208135924e
riffgriff <griffinmartin02@gmail.com>
2026-04-24 15:40:40 -0400

    param tuning

--- src/galactic_maze_nav/galactic_maze_nav/navigate_maze.py
+++ src/galactic_maze_nav/galactic_maze_nav/navigate_maze.py
@@ -57,8 +57,8 @@ class MazeNavigator(Node):
 		self.declare_parameter('kp', 0.005)
 		self.declare_parameter('ki', 0.0)
 		self.declare_parameter('kd', 0.0001)
-		self.declare_parameter('img_size_steering_threshold', 1000)
-		self.declare_parameter('img_steering_borders', 50)
+		self.declare_parameter('img_size_steering_threshold', 2000)
+		self.declare_parameter('img_steering_borders', 70)
 
 		self.model_path = self.get_parameter('model_path').value
 		self.knn_k = int(self.get_parameter('knn_k').value)
@@ -198,7 +198,7 @@ class MazeNavigator(Node):
 				angular_effort = 0.0
 				if self.img_pos is not None:
 					# check if size is within threshold
-					if img_preprocessing.is_reasonable_box(self.img_pos[2], self.img_pos[3], min_area = 1000):
+					if img_preprocessing.is_reasonable_box(self.img_pos[2], self.img_pos[3], min_area = self.img_size_steering_threshold):
 						pixel_error = 160 - (self.img_pos[0] + self.img_pos[2]/2) # 320 pix wide camera resolution
 						# check if the image is relatively centered
 						if abs(pixel_error) < 160 - self.img_steering_borders:
